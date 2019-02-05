0810864501e11b605af45ecc9795119ee4a3e68c
mmohamedaadel <mmohamedaadel@gmail.com>
2019-02-05 13:53:02 -0600

    Add files via upload

---
+++ src/main/java/com/example/gradledemo/config/UserDetails.java
@@ -0,0 +1,34 @@
+package com.example.gradledemo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    private String user;
+    private String pass;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
---
+++ src/main/java/com/example/gradledemo/controller/TestController.java
@@ -0,0 +1,16 @@
+//package com.example.gradledemo.controller;
+//
+//import org.springframework.security.access.annotation.Secured;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class TestController {
+//
+//    @Secured("ADMIN")
+//    @GetMapping("/")
+//    public String test() {
+//        return "hello0";
+//    }
+//}
